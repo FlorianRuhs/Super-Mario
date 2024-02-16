591b33d2c649606a943714a74ca62ca359372409
Florian Ruhs <florian.ruhs@ing.com>
2024-02-16 15:20:55 +0100

    Update

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -33,6 +33,7 @@ public class GamePanel extends JPanel implements Runnable{
     Sound se = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
+    public UI ui = new UI(this);
     Thread gameThread;
 
     // Entity and Object
@@ -117,6 +118,10 @@ public class GamePanel extends JPanel implements Runnable{
 
         // Player
         player.draw(g2);
+
+        // UI
+        ui.draw(g2);
+
         g2.dispose();
     }
    public void playMusic(int i) {
